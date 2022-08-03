ac194c45876649408228e4efcc203971a34fffda
Taiwo Adeola Thorpe <adesokantaiwo@yahoo.com>
2022-08-04 07:47:43 +0800

    Update on User - authentication

---
+++ excel_comp/accounts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+
+
+urlpatterns = [
+	path('register/', views.registerPage, name="register"),
+	path('login/', views.loginPage, name="login"),  
+	path('logout/', views.logoutUser, name="logout"),
+	path('homepage/', views.homepage, name='home')
+
+]
--- excel_comp/accounts/views.py
+++ excel_comp/accounts/views.py
@@ -1,3 +1,64 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
+from django.http import HttpResponse
+from django.forms import inlineformset_factory
+from django.contrib.auth.forms import UserCreationForm
+
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib import messages
+
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+from .models import *
+from .forms import CreateUserForm
+import homepage 
+
+
+def registerPage(request):
+	if request.user.is_authenticated:
+		return redirect('homepage')
+	else:
+		form = CreateUserForm()
+		if request.method == 'POST':
+			form = CreateUserForm(request.POST)
+			if form.is_valid():
+				form.save()
+				user = form.cleaned_data.get('username')
+				messages.success(request, 'Account was created for ' + user)
+
+				return redirect('login')
+			
+
+		context = {'form':form}
+		return render(request, 'accounts/register.html', context)
+
+def loginPage(request):
+	if request.user.is_authenticated:
+		return redirect('homepage')
+	else:
+		if request.method == 'POST':
+			username = request.POST.get('username')
+			password =request.POST.get('password')
+
+			user = authenticate(request, username=username, password=password)
+
+			if user is not None:
+				login(request, user)
+				return redirect('homepage')
+			else:
+				messages.info(request, 'Username OR password is incorrect')
+
+		context = {}
+		return render(request, 'accounts/login.html', context)
+
+def logoutUser(request):
+	logout(request)
+	return redirect('login')
+
+
+# def home(request):
+#     return render(request, "home.html")
+
+def homepage(request):
+    return render(request, "home.html")
