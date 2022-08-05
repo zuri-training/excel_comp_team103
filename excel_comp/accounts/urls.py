240bf02121d3e5144552eef79f2190023f7a3b7d
Nnaemeka Joseph <nnaemekaxi@yahoo.com>
2022-08-05 20:54:00 +0100

    links updated

--- excel_comp/accounts/urls.py
+++ excel_comp/accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from accounts import views
 
+app_name = 'accounts'
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
--- excel_comp/contact_page/urls.py
+++ excel_comp/contact_page/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'contact_page'
 urlpatterns = [
     path('', views.contact_us, name='contact_us'),
 ]
