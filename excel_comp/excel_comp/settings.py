52038ae94d9265e113407522fe933042371d8552
Simon Oladeji <simmax456@gmail.com>
2022-08-15 18:27:16 +0100

    Automatic configuration with django-heroku

--- excel_comp/excel_comp/settings.py
+++ excel_comp/excel_comp/settings.py
@@ -136,3 +136,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR,'homepage/static'),]
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import django_heroku
+django_heroku.settings(locals())
